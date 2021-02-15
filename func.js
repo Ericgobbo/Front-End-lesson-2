234f3d7cd049110c6ee9515ed803129cd8574404
Eric Gobbo <33002736+Ericgobbo@users.noreply.github.com>
2021-02-15 16:32:30 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,20 @@
+$(function () {
+  $("h2").click(function () {
+
+    if ($(this).next("p").is(":visible")) {
+      $("p").hide();
+      $("span").css({'transform': 'rotate(0deg)'});
+    } else {
+      $("p").hide();
+      $("h2").css("font-weight", "400");
+      $(this).next("p").show();
+      $(this).find("span").css({'transform': 'rotate(-180deg)'});
+    }
+
+    if ($(this).css("font-weight") == "400") {
+      $(this).css("font-weight", "700");
+    } else {
+      $(this).css("font-weight", "400");
+    }
+  });
+});
